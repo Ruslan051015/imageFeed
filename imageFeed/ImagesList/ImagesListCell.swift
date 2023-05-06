a2e8c1a44bda895eca6706f804a5a59150ecc09b
ruslan0515015 <ruslaniosdev@gmail.com>
2023-05-07 02:28:50 +0400

    Возникла ошибка с отображением картинок

--- imageFeed/ImagesList/ImagesListCell.swift
+++ imageFeed/ImagesList/ImagesListCell.swift
@@ -8,4 +8,5 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
+   
 }
--- imageFeed/ImagesList/ImagesListViewController.swift
+++ imageFeed/ImagesList/ImagesListViewController.swift
@@ -3,33 +3,73 @@ import UIKit
 class ImagesListViewController: UIViewController {
     //MARK: - Properties:
     private let photosName: [String] = Array(0..<20).map { "\($0)" }
-    private lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
+    
     //MARK: - Outlets:
     @IBOutlet private var tableView: UITableView!
     
     //MARK: - Lifecycle:
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0 )
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
-    //MARK: - Methods:
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
+
+//MARK: - Extensions:
+extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        let photoName = photosName[indexPath.row]
-        cell.cellImage?.image = UIImage(named: photoName)
-        cell.dateLabel?.text = dateFormatter.string(from: Date())
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
         if indexPath.row % 2 == 0 {
             cell.likeButton.imageView?.image = UIImage(named: "Like pressed")
         } else {
             cell.likeButton.imageView?.image = UIImage(named: "Like not pressed")
         }
     }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("Failed to create object of ImageListCell type")
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
     
     
 }
+
 
