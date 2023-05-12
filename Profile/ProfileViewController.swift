0b467d22c42a7bfe78b8405e2d9d5ce8c3d65ef3
ruslan0515015 <ruslaniosdev@gmail.com>
2023-05-13 02:26:38 +0400

    Created Profile Screen and added some of the elements of UI into itself

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+  //MARK: - Properties:
+    override  var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    // MARK: - Outlets:
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    //MARK: - Actions:
+    @IBAction func logOutAction(_ sender: Any) {
+    }
+    //MARK: - LifyCycle:
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
