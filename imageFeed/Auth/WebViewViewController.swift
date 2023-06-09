bf4b6b3edee9d8a85b818fa6bc534be4883c0e43
ruslan0515015 <ruslaniosdev@gmail.com>
2023-06-09 16:15:44 +0400

    Added ProgressView to the webView and set up it's update during webView loading, what is KVO actually means as i got it

--- imageFeed/Auth/WebViewViewController.swift
+++ imageFeed/Auth/WebViewViewController.swift
@@ -13,6 +13,14 @@ final class WebViewViewController: UIViewController {
     @IBAction func didTapBackButton(_ sender: Any) {
     }
     // MARK: - LifeCycle:
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.navigationDelegate = self
@@ -25,8 +33,32 @@ final class WebViewViewController: UIViewController {
             URLQueryItem(name: "scope", value: AccesScope)
         ]
         let url = urlComponents.url!
-        
         let request = URLRequest(url: url)
         webView.load(request)
+        
+        updateProgress()
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        webView.removeObserver(self,
+                               forKeyPath: #keyPath(WKWebView.estimatedProgress),
+                               context: nil)
+    }
+    // MARK: - Methods:
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
     }
 }
