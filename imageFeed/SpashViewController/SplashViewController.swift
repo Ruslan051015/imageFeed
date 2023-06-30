1ef0ba49f98b9c7b4016f13c0e48e70863a9f201
ruslan0515015 <ruslaniosdev@gmail.com>
2023-07-01 02:11:27 +0400

    Added blocking UI while authorization by UIBlockingProgressHUD

--- imageFeed/SpashViewController/SplashViewController.swift
+++ imageFeed/SpashViewController/SplashViewController.swift
@@ -39,7 +39,7 @@ final class SplashViewController: UIViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -51,9 +51,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
                 // TODO [Sprint 11]
                 break
             }
